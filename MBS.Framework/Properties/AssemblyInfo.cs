5ce5151dfef140385d4a5b65ff402a89ce332408
Michael Becker <alcexhim@gmail.com>
2020-10-15 00:15:14 -0400

    we really should try to be CLS-compliant

--- MBS.Framework/Properties/AssemblyInfo.cs
+++ MBS.Framework/Properties/AssemblyInfo.cs
@@ -18,6 +18,7 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -44,3 +45,4 @@ using System.Runtime.CompilerServices;
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
+[assembly: CLSCompliant(true)]
