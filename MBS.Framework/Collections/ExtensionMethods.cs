de9149ea8cc471ddddfdd32a46a284d0c7e71fad
Michael Becker <alcexhim@gmail.com>
2021-08-14 23:45:19 -0400

    implement ToNullTerminatedArray<> and ToArray<> extension methods, as well as a handy HandleDictionary

---
+++ MBS.Framework/Collections/ExtensionMethods.cs
@@ -0,0 +1,50 @@
+//
+//  ExtensionMethods.cs - implements generic-typed extension methods for System.Collections classes
+//
+//  Author:
+//       Michael Becker <alcexhim@gmail.com>
+//
+//  Copyright (c) 2020 Mike Becker's Software
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Collections;
+
+namespace MBS.Framework.Collections
+{
+	public static class ExtensionMethods
+	{
+		public static T[] ToArray<T>(this IEnumerable enumerable) where T : class
+		{
+			System.Collections.Generic.List<T> list = new System.Collections.Generic.List<T>();
+			foreach (object name in enumerable)
+			{
+				if (name is T)
+					list.Add((T)name);
+			}
+			return list.ToArray();
+		}
+		public static T[] ToNullTerminatedArray<T>(this IEnumerable enumerable) where T : class
+		{
+			System.Collections.Generic.List<T> list = new System.Collections.Generic.List<T>();
+			foreach (object name in enumerable)
+			{
+				if (name is T)
+					list.Add((T)name);
+			}
+			list.Add(null);
+			return list.ToArray();
+		}
+	}
+}
---
+++ MBS.Framework/Collections/Generic/HandleDictionary.cs
@@ -0,0 +1,42 @@
+//
+//  HandleDictionary.cs
+//
+//  Author:
+//       Michael Becker <alcexhim@gmail.com>
+//
+//  Copyright (c) 2021 Mike Becker's Software
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+namespace MBS.Framework.Collections.Generic
+{
+	public class HandleDictionary<TObject>
+	{
+		private BidirectionalDictionary<IntPtr, TObject> _dict = new BidirectionalDictionary<IntPtr, TObject>();
+
+		public TObject GetObject(IntPtr handle)
+		{
+			return _dict.GetValue2(handle);
+		}
+		public IntPtr GetHandle(TObject obj)
+		{
+			return _dict.GetValue1(obj);
+		}
+
+		public void Add(IntPtr handle, TObject obj)
+		{
+			_dict.Add(handle, obj);
+		}
+	}
+}
