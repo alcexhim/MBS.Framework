36115d61fa585d89f73a958fde5cf7148792d8ba
Michael Becker <alcexhim@gmail.com>
2022-03-10 16:34:36 -0500

    Merge branch 'master' of github.com:alcexhim/MBS.Framework

---
+++ MBS.Framework/NativeHandle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace MBS.Framework
+{
+	public class NativeHandle
+	{
+	}
+	public class NativeHandle<THandle> : NativeHandle
+	{
+		public THandle Handle { get; private set; }
+
+		public NativeHandle(THandle handle)
+		{
+			Handle = handle;
+		}
+
+		public override int GetHashCode()
+		{
+			return Handle.GetHashCode();
+		}
+		public override bool Equals(object obj)
+		{
+			if ((object)obj == null && (object)this == null)
+				return true;
+			if ((object)obj == null || (object)this == null)
+				return false;
+
+			if (obj is NativeHandle<THandle>)
+				return Equals(obj as NativeHandle<THandle>);
+
+			return false;
+		}
+
+		public static bool operator ==(NativeHandle<THandle> left, NativeHandle<THandle> right)
+		{
+			if ((object)left == null && (object)right == null)
+				return true;
+			if ((object)left == null || (object)right == null)
+				return false;
+
+			return left.Equals(right);
+		}
+		public static bool operator !=(NativeHandle<THandle> left, NativeHandle<THandle> right)
+		{
+			return !left.Equals(right);
+		}
+	}
+}
