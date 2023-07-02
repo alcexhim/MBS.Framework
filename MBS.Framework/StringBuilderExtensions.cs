88a27eb26facebba0cec01c609d259bf2498425f
Michael Becker <alcexhim@gmail.com>
2023-07-01 21:20:13 -0400

    add StringBuilder extensions and ICustomPlugin, and move ProgressEvent from UniversalEditor into MBS.Framework core

---
+++ MBS.Framework/StringBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace MBS.Framework
+{
+	public static class StringBuilderExtensions
+	{
+		public static void AppendLineIndented(this StringBuilder sb, object value, int indentLevel, char indentChar = '\t', int indentCharCountPerLevel = 1)
+		{
+			for (int i = 0; i < indentLevel * indentCharCountPerLevel; i++)
+			{
+				sb.Append(indentChar);
+			}
+			sb.Append(value);
+			sb.AppendLine();
+		}
+	}
+}
